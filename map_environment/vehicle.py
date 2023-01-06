551898c1edec2402d6fd798be2f8b4f67358a2b7
Kenny Brandon <K.F.Brandon@student.tudelft.nl>
2023-01-06 17:13:26 +0100

    Added a simulation class for the vehicle (still need to test it with values)

--- map_environment/vehicle.py
+++ map_environment/vehicle.py
@@ -49,3 +49,69 @@ class BicycleModel:
         ddq = sol[0:3]
 
         return ddq
+
+
+
+'SIMULATION'
+
+import pygame
+import math
+
+# Set up Pygame
+pygame.init()
+
+# Set up the window
+width, height = 550, 550
+screen = pygame.display.set_mode((width, height))
+
+# Draw the path on the screen
+path_color = (0, 0, 255)
+path_width = 3
+pygame.draw.line(screen, path_color, (0, 0), ([100, 100]), path_width)
+
+
+# Define the Car class
+class Car:
+    def __init__(self, x, y, v, phi, a, delta):
+        self.x = x # x-position of the vehicle
+        self.y = y # y-position of the vehicle
+        self.v = v # velocity of the vehicle
+        self.phi = phi # orientation angle of the vehicle
+        self.acc = a # acceleration of the vehicle
+        self.delta = delta # orientation angle of the front wheels of the vehicle
+
+        self.m = 0.5
+        self.L = 1
+        self.I = (1/12)*self.m*self.L**2
+        self.a = self.L/2
+        self.b = self.L - self.a
+
+
+
+    def update(self):
+        # Update the position and velocity of the car using the equations of motion
+        self.x += self.v * math.cos(self.phi)
+        self.y += self.v * math.sin(self.phi)
+        self.v += self.acc
+        self.phi += self.v * math.tan(self.delta) / self.L
+
+        
+# Create an instance of the Car class
+car = Car(0, 0, 0, 0, 0, 0)
+#car = Car(x_mpc,y_mpc,v_mpc,phi_mpc,a_mpc,delta_mpc) # The state-vector values that come from the MPC
+
+
+# Main loop
+while True:
+    # Update the car's position and velocity
+    car.update()
+
+    # Draw the car on the screen
+    car_color = (255, 0, 0)
+    car_size = (20, 30)
+    car_rect = pygame.Rect(car.x, car.y, car_size[0], car_size[1])
+    pygame.draw.rect(screen, car_color, car_rect)
+
+    # Update the Pygame display
+    pygame.display.flip()
+
