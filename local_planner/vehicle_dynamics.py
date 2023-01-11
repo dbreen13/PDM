e8e5c54ad6b0245a6470e6436e825ddae79bd109
Joris Weeda <j.j.weeda@student.tudelft.nl>
2023-01-11 17:18:45 +0100

    Updated mpc controller and vehicle dynamics to be based on the report statistics

--- local_planner/vehicle_dynamics.py
+++ local_planner/vehicle_dynamics.py
@@ -3,38 +3,46 @@ import numpy as np
 
 class VehicleDynamics:
     SAMPLING_TIME = 0.10
-    WHEEL_BASE_LENGTH = 2.5
+    WHEEL_BASE_LENGTH = 7.5
 
-    MAX_STEER_SPEED = np.deg2rad(45.0)  # maximum steering speed [rad/s]
+    TARGET_SPEED = 10.0
+
     MAX_STEER_ANGLE = np.deg2rad(60.0)  # maximum steering angle [rad]
-    MAX_SPEED = 5.1  # maximum speed [m/s]
-    MIN_SPEED = 0.0  # minimum speed [m/s]
-    MAX_ACCEL = 2.0  # maximum accel [m/ss]
+    MAX_SPEED = 15.  # maximum speed [dm/s]
+    MIN_SPEED = 0.0  # minimum speed [dm/s]
+    MAX_ACCEL = 0.75  # maximum accel [dm/ss]
 
     def __init__(self, x_pos=0.0, y_pos=0.0, yaw=0.0, velocity=0.0, sampling_time=SAMPLING_TIME):
         self.dt = sampling_time
 
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.velocity = velocity
+        self.vel = velocity
         self.yaw = yaw
 
         self.A = np.eye(4)
         self.B = np.zeros((4, 2))
         self.C = np.zeros(4)
 
+    def reset_state(self):
+        """ """
+        self.x_pos = 0.
+        self.y_pos = 0.
+        self.vel = 0.
+        self.yaw = 0.
+
     def get_current_state(self):
-        return np.array([self.x_pos, self.y_pos, self.velocity, self.yaw])
+        return np.array([self.x_pos, self.y_pos, self.vel, self.yaw])
 
     def update_state(self, control_vec):
         """ """
         acceleration, steering_angle_delta = control_vec
 
-        self.x_pos += (self.velocity * np.cos(self.yaw)) * self.dt
-        self.y_pos += (self.velocity * np.sin(self.yaw)) * self.dt
-        self.velocity += acceleration * self.dt
+        self.x_pos += (self.vel * np.cos(self.yaw)) * self.dt
+        self.y_pos += (self.vel * np.sin(self.yaw)) * self.dt
+        self.vel += acceleration * self.dt
 
-        self.yaw += self.velocity / self.WHEEL_BASE_LENGTH * np.tan(steering_angle_delta) * self.dt
+        self.yaw += self.vel / self.WHEEL_BASE_LENGTH * np.tan(steering_angle_delta) * self.dt
         self.yaw = self.normalize_angle(self.yaw)
 
     def prediction_motion(self, ctrl_acc, ctrl_delta, reference_x, mpc_horizon):
@@ -42,7 +50,7 @@ class VehicleDynamics:
         x_prediction = np.zeros(reference_x.shape)
 
         current_x_pos, current_y_pos = self.x_pos, self.y_pos
-        current_yaw, current_vel = self.yaw, self.velocity
+        current_yaw, current_vel = self.yaw, self.vel
 
         x_prediction[0, 0] = current_x_pos
         x_prediction[1, 0] = current_y_pos
@@ -55,11 +63,11 @@ class VehicleDynamics:
 
             x_prediction[0, idx] = self.x_pos
             x_prediction[1, idx] = self.y_pos
-            x_prediction[2, idx] = self.velocity
+            x_prediction[2, idx] = self.vel
             x_prediction[3, idx] = self.yaw
 
         self.x_pos, self.y_pos = current_x_pos, current_y_pos
-        self.yaw, self.velocity = current_yaw, current_vel
+        self.yaw, self.vel = current_yaw, current_vel
 
         return x_prediction
 
